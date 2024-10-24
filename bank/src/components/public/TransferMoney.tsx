5ea5cd4d33fe8044b04bc7018a39e1948889cf4a
jangkiwoong <wkdrldnd99@gmail.com>
2024-10-24 17:48:43 +0900

    [Feat] : 초기화 버튼 생성

--- bank/src/components/public/TransferMoney.tsx
+++ bank/src/components/public/TransferMoney.tsx
@@ -24,18 +24,43 @@ const TransferMoney = () => {
         transferData();
         window.location.reload();
     }
+    const revers = () => {
+        const transferData = async () =>{
+            try{
+                    await axios.put("http://localhost:8080/api/update/send/money",
+                    {
+                        myAccount: "0000-0000-0000",
+                        relativeAccount: "1111-1111-1111",
+                        transferMoney: 1000, // 예시 송금액
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+
+            } catch (e) {
+                console.error("요청 에러 : ", e);
+            }
+        };
+        transferData();
+        window.location.reload();
+    }
     return(
         <Container>
             <TransferBtn onClick={transfer}>송금</TransferBtn>
+            <Revers onClick={revers}>되돌리기</Revers>
         </Container>
     );
 }
 export default TransferMoney;
 
 const Container = styled.div`
-    margin-top: 10px
-    
+    margin-top: 10px;
 `
 const TransferBtn = styled.button`
     
+`
+const Revers = styled.button`
+    margin-left: 10px;
 `
