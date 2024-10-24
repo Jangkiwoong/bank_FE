6ff5da65390b3541c1c0b00220ff32a99d399b34
jangkiwoong <wkdrldnd99@gmail.com>
2024-10-24 15:19:10 +0900

    [Refactor] : 컨포넌트 분리

---
+++ bank/src/components/public/GetAccount.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Account {
+    id: number;
+    account: string;
+    money: number;
+}
+interface ApiResponse {
+    message: string;
+    data: Account[];
+}
+
+const GetAccount = () => {
+    const [accounts, setAccounts] = useState<ApiResponse | null>(null);
+
+    useEffect(() => {
+        const accountData = async () =>{
+            try{
+                const response = await axios.get<ApiResponse>("http://localhost:8080/api/get/all/account");
+                setAccounts(response.data);
+            } catch (e) {
+                console.error("요청 에러 : ", e);
+                setAccounts(null);
+            }
+        };
+        accountData();
+    },[])
+
+    return(
+        <Container>
+            {accounts?.data.map(accounts => (
+                <AccountContainer key={accounts.id}>
+                    <Account >ID : {accounts.id}</Account>
+                    <Account >계좌번호 : {accounts.account}</Account>
+                    <Account >보유잔액 : {accounts.money}</Account>
+                </AccountContainer>
+            ))}
+        </Container>
+    );
+}
+export default GetAccount;
+const Container = styled.div`
+    width: 30%;
+`;
+const Account = styled.p`
+
+`
+const AccountContainer = styled.div`
+    display: flex;
+    flex-direction: row; /* 세로 정렬 */
+    align-items: center;
+    justify-content: space-between;
+
+    border: 1px solid white;
+    border-radius: 10px;
+    gap: 30px;
+    margin-top: 15px;
+    padding: 20px; /* 여백 추가 */
+`
--- bank/src/page/Home.tsx
+++ bank/src/page/Home.tsx
@@ -1,42 +1,10 @@
 import styled from "styled-components";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-interface Account {
-    id: number;
-    account: string;
-    money: number;
-}
-interface ApiResponse {
-    message: string;
-    data: Account[];
-}
+import GetAccount from "../components/public/GetAccount";
 
 const Home = () => {
-    const [accounts, setAccounts] = useState<ApiResponse | null>(null);
-
-    useEffect(() => {
-        const accountData = async () =>{
-            try{
-                const response = await axios.get<ApiResponse>("http://localhost:8080/api/get/all/account");
-                setAccounts(response.data);
-            } catch (e) {
-                console.error("요청 에러 : ", e);
-                setAccounts(null);
-            }
-        };
-        accountData();
-    },[])
-
     return(
         <Container>
-            {accounts?.data.map(accounts => (
-                <AccountContainer key={accounts.id}>
-                    <Account >ID : {accounts.id}</Account>
-                    <Account >계좌번호 : {accounts.account}</Account>
-                    <Account >보유잔액 : {accounts.money}</Account>
-                </AccountContainer>
-            ))}
+            <GetAccount />
         </Container>
     );
 }
@@ -44,22 +12,8 @@ export default Home;
 
 const Container = styled.div`
     display: flex;
-    justify-content: center;  /* 가로 중앙 정렬 */
-    align-items: center;      /* 세로 중앙 정렬 */
-    height: 100vh;            /* 화면 전체 높이 */
-    flex-direction: column;
-`
-const Account = styled.p`
-    padding: 10px;
-`
-const AccountContainer = styled.div`
-      border: 1px solid white;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;               /* 적절한 너비 설정 */
-  max-width: 800px;
-  /* padding: 15px; */
-  gap: 30px;
+    justify-content: center;  /* 수평 가운데 정렬 */
+    align-items: center;      /* 수직 가운데 정렬 */
+    width: 420%;               /* 수정 필요 */
+    height: 10%;               /* 수정 필요 */
 `
