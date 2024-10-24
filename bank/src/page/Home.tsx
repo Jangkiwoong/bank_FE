a4c7c972fbdfa9ae8de7fee3e234a3fb4863b052
jangkiwoong <wkdrldnd99@gmail.com>
2024-10-24 14:36:03 +0900

    [Refactor] : 오타 수정

--- bank/src/page/Home.tsx
+++ bank/src/page/Home.tsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-interface Accounts {
+interface Account {
     id: number;
     account: string;
     money: number;
 }
 interface ApiResponse {
     message: string;
-    data: Accounts[];
+    data: Account[];
 }
 
 const Home = () => {
@@ -18,7 +18,7 @@ const Home = () => {
     useEffect(() => {
         const accountData = async () =>{
             try{
-                const response = await axios.get<ApiResponse>("http://localhost:8080/get/all/accounts");
+                const response = await axios.get<ApiResponse>("http://localhost:8080/api/get/all/accounts");
                 setAccounts(response.data);
             } catch (e) {
                 console.error("요청 에러 : ", e);
@@ -31,11 +31,11 @@ const Home = () => {
     return(
         <Container>
             {accounts?.data.map(accounts => (
-                <AccountsContainer key={accounts.id}>
-                    <Accounts >{accounts.id}</Accounts>
-                    <Accounts >{accounts.account}</Accounts>
-                    <Accounts >{accounts.money}</Accounts>
-                </AccountsContainer>
+                <AccountContainer key={accounts.id}>
+                    <Account >ID : {accounts.id}</Account>
+                    <Account >계좌번호 : {accounts.account}</Account>
+                    <Account >보유잔액 : {accounts.money}</Account>
+                </AccountContainer>
             ))}
         </Container>
     );
@@ -43,9 +43,23 @@ const Home = () => {
 export default Home;
 
 const Container = styled.div`
-    
+    display: flex;
+    justify-content: center;  /* 가로 중앙 정렬 */
+    align-items: center;      /* 세로 중앙 정렬 */
+    height: 100vh;            /* 화면 전체 높이 */
+    flex-direction: column;
 `
-const Accounts = styled.p`
+const Account = styled.p`
+    padding: 10px;
 `
-const AccountsContainer = styled.div`
+const AccountContainer = styled.div`
+      border: 1px solid white;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;               /* 적절한 너비 설정 */
+  max-width: 800px;
+  /* padding: 15px; */
+  gap: 30px;
 `
