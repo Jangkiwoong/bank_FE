4bc24f4353231937e52784b267c151c80aede3e9
jangkiwoong <wkdrldnd99@gmail.com>
2024-11-05 23:44:52 +0900

    Flex

--- bank/src/page/TestFlex.tsx
+++ bank/src/page/TestFlex.tsx
@@ -1,41 +1,52 @@
 import styled from "styled-components";
-import GetAccount from "../components/public/GetAccount";
-import TransferMoney from "../components/public/TransferMoney";
 
 const Home = () => {
-    return(
+    return (
         <div>
-            <Container>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            </Container>
+            <CenteredContainer>
+                <Container>
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                    <Box />
+                </Container>
+            </CenteredContainer>
         </div>
     );
 }
+
 export default Home;
+
+const CenteredContainer = styled.div`
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height:100%;
+`;
 
 const Container = styled.div`
     border: 1px solid black;
-
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     flex-wrap: wrap;
-    justify-content: flex-start;
-    align-content: space-evenly;
-    
-    width: 59.6%;
-    height: 500px;
-`
+    justify-content: flex-start; /* 왼쪽 정렬 */
+    width: 50%;
+    height: 30%;
+`;
+
 const Box = styled.div`
+    flex-shrink: 1;
+    align-self: stretch;
     width: 100px;
     height: 100px;
     margin: 5px;
     border: 1px solid black;
-`
+`;
